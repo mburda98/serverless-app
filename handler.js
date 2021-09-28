05f4529647817748deac64a6f55b8db1a89415b7
mburda98 <m.burda0805@gmail.com>
2021-09-28 10:59:38 +0200

    init commit

---
+++ handler.js
@@ -0,0 +1,21 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  if (Math.random() > 0.5) {
+    return res.status(400).json({ message: "Bad request" });
+  }
+
+  return res.status(200).json({
+    message: "Hello!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
